ea5094180181ef37ac86a379c603a69d38cb1612
Ruben Suarez <ruben.suarezdz@udlap.mx>
2024-09-30 00:24:23 -0600

    Implementing GUARDS

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, GuardResult, MaybeAsync, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({providedIn: 'root'})
+export class AuthGuard implements CanMatch, CanActivate{
+  constructor(
+    private authService:AuthService,
+    private router:Router
+  ) { }
+
+  private checkOutStatus():boolean | Observable<boolean>{
+    return this.authService.checkAuthentication()
+      .pipe(
+        tap( isAuthenticated => {
+          if (!isAuthenticated) this.router.navigate(['./auth/login'])
+        }),
+
+      )
+  }
+
+
+  canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean>  {
+    // throw new Error('Method not implemented.');
+
+    return this.checkOutStatus();
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
+    // throw new Error('Method not implemented.');
+    return this.checkOutStatus();
+  }
+
+}
